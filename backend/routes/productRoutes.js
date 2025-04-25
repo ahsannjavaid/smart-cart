94639225734641d2d44d916818655d0d5a547bdf
Ahsan Javed <ahsannjavaid@gmail.com>
2025-04-25 12:23:58 +0500

    Get Products Controller Added

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/productController");
 
-router.get("/products", controller.getProducts);
+router.get("/getproducts", controller.getProducts);
 router.post("/addProduct", controller.addProduct);
 
 module.exports = router;
