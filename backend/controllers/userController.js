2bdd6b186426103ed894f63cb52e315822fe99e0
WaleedAnsari7364 <waleedansari7364@gmail.com>
2025-04-26 21:54:25 +0500

    Update Password Controller Added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -41,3 +41,17 @@ exports.signup = async (req, res) => {
   u.save()
   res.status(200).json({ success: "Success" })
 }
+
+exports.updatePassword = async (req, res) => {
+  let token = req.body.token
+  let user = jwt.verify(token, process.env.JWT_SECRET)
+  let dbuser = await User.findOne({ email: user.email })
+  const bytes = CryptoJS.AES.decrypt(dbuser.password, process.env.AES_SECRET);
+  let decryptedPass = bytes.toString(CryptoJS.enc.Utf8);
+  if (decryptedPass == req.body.password && req.body.npassword == req.body.cpassword) {
+    let dbuser = await User.findOneAndUpdate({ email: user.email }, { password: CryptoJS.AES.encrypt(req.body.cpassword, process.env.AES_SECRET).toString() })
+
+    res.status(200).json({ success: true });
+  }
+  res.status(200).json({ success: false });
+}
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,5 +5,6 @@ const controller = require("../controllers/userController");
 router.get("/getuser", controller.getUser);
 router.post("/login", controller.login);
 router.post("/signup", controller.signup);
+router.post("/updatepassword", controller.updatePassword);
 
 module.exports = router;
